4d3cc48551e9da3f70e2d5119a3331aff19176d4
huangyCode <254700023@qq.com>
2020-07-22 03:55:38 +0800

    init

--- app/government/view/ServiceDetail/index.js
+++ app/government/view/ServiceDetail/index.js
@@ -1,11 +1,31 @@
 import React, {PureComponent} from 'react';
 import './index.scss'
+import cheng from '../../static/img/icon_chengxiangshebao.png'
 
 
 export default class ServiceDetail extends PureComponent {
     render() {
         return <div>
-            <div></div>
+            <div className='head' >
+                <img className='icon' src={cheng}/>
+                <div className='name'>
+                    xxxx综合服务
+                </div>
+            </div>
+            <div className='desc'>
+                <div className='l'></div>
+                <div className='desc-title'>服务信息</div>
+                <div className='desc-body'>
+                    asdasdasdasd
+                </div>
+            </div>
+            <div className='desc'>
+                <div className='l'></div>
+                <div className='desc-title'>服务窗口</div>
+                <div className='item'>5号窗口</div>
+                <div className='item'>4号窗口</div>
+                <div className='item'>3号窗口</div>
+            </div>
         </div>
     }
 }
--- app/government/view/ServiceList/index.js
+++ app/government/view/ServiceList/index.js
@@ -1,24 +1,93 @@
 import React, {PureComponent} from 'react';
 import './index.scss'
-
+import cheng from '../../static/img/icon_chengxiangshebao.png'
 
 export default class ServiceList extends PureComponent {
-    render(){
+    render() {
         return <div>
             <div className='group'>
                 <div className='title'>
-
+                    社区
                 </div>
-                <div className='item'>
-                    <img className='icon' src={}/>
-                    <div className='item-name'>
-                        name
+                <div className='group-body'>
+                    <div className='item'>
+                        <img className='icon' src={cheng}/>
+                        <div className='item-name'>
+                            name
+                        </div>
+                    </div>
+                    <div className='item'>
+                        <img className='icon' src={cheng}/>
+                        <div className='item-name'>
+                            name
+                        </div>
+                    </div>
+                    <div className='item'>
+                        <img className='icon' src={cheng}/>
+                        <div className='item-name'>
+                            name
+                        </div>
+                    </div>
+                    <div className='item'>
+                        <img className='icon' src={cheng}/>
+                        <div className='item-name'>
+                            name
+                        </div>
+                    </div>
+                    <div className='item'>
+                        <img className='icon' src={cheng}/>
+                        <div className='item-name'>
+                            name
+                        </div>
+                    </div>
+                    <div className='item'>
+                        <img className='icon' src={cheng}/>
+                        <div className='item-name'>
+                            name
+                        </div>
                     </div>
                 </div>
-                <div className='item'>
-                    <img className='icon' src={}/>
-                    <div className='item-name'>
-                        name
+            </div>
+            <div className='group'>
+                <div className='title'>
+                    社区
+                </div>
+                <div className='group-body'>
+                    <div className='item'>
+                        <img className='icon' src={cheng}/>
+                        <div className='item-name'>
+                            name
+                        </div>
+                    </div>
+                    <div className='item'>
+                        <img className='icon' src={cheng}/>
+                        <div className='item-name'>
+                            name
+                        </div>
+                    </div>
+                    <div className='item'>
+                        <img className='icon' src={cheng}/>
+                        <div className='item-name'>
+                            name
+                        </div>
+                    </div>
+                    <div className='item'>
+                        <img className='icon' src={cheng}/>
+                        <div className='item-name'>
+                            name
+                        </div>
+                    </div>
+                    <div className='item'>
+                        <img className='icon' src={cheng}/>
+                        <div className='item-name'>
+                            name
+                        </div>
+                    </div>
+                    <div className='item'>
+                        <img className='icon' src={cheng}/>
+                        <div className='item-name'>
+                            name
+                        </div>
                     </div>
                 </div>
             </div>
